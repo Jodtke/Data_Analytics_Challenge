fa5dff85adf92fb50712d04debe33ce787941107
Jodtke <ejahnke13@t-online.de>
2021-05-18 17:13:57 +0200

    neues csv file mit joined items & transactions

--- R-Skripte/Author_Strings.R
+++ R-Skripte/Author_Strings.R
@@ -1,10 +1,10 @@
 ######### Main Topics auf 3 Buchstaben kürzen ############
 
-# libraries
+## libraries
 library(tidyverse)
 library(janitor)
 
-# daten
+## daten
 transactions_raw <- read.csv(file = "./Data/transactions.csv", header = T, sep = "|", quote = "", row.names = NULL, stringsAsFactors = F)
 dim(transactions_raw)  #365 143 x 5
 openRefine <- read.csv(file = "./Data/items_bearbeitet5.csv", header = T, sep = ",", row.names = NULL, stringsAsFactors = F, encoding = "UTF-8")
@@ -15,12 +15,12 @@ joined_oR <- left_join(oR_tbl, transactions_tbl, by = "itemID")
 head(joined_oR, n=10)
 rm(openRefine, oR_tbl, transactions_raw, transactions_tbl)
 
-# einzelne characters pro string zählen mit 'nchar' (base R)
+## einzelne characters pro string zählen mit 'nchar' (base R)
 nchar(joined_oR$main.topic[[1]]) # 3 characters
 joined_oR$main.topic[nchar(joined_oR$main.topic) > 3] # zum Beispiel: WZSN
 joined_oR$main.topic[joined_oR$main.topic == "WZSN"] # ziemlich viele Fälle!
 
-# Trimmen der Main Topics auf max. 3 Stellen mit str_trunc des strinR Package aus tidyverse & sapply()
+## Trimmen der Main Topics auf max. 3 Stellen mit str_trunc des strinR Package aus tidyverse & sapply()
 joined_oR <- joined_oR %>%
   mutate(
     main.topic = sapply(main.topic, function(x) if_else(nchar(x) > 3, str_trunc(x, width=3), x)),
@@ -31,6 +31,22 @@ joined_oR <- joined_oR %>%
 joined_oR %>% select(main.topic) %>% filter(nchar(main.topic) > 3) # 0 Fälle von MainTopic mit 4 characters pro String
 joined_oR %>% select(main.topic) %>% filter(main.topic == "WZSN") # 0 Fälle von WZSN in MainTopic
 joined_oR %>% select(main.topic) %>% filter(main.topic == "WZS") # viele Fälle von WZS --> Check!
+
+## neue csv schreiben
+write_csv(joined_oR, file="./Data/joined_item_trans.csv", col_names=T)
+joined_item_trans <- read_csv(file = "./Data/joined_item_trans.csv", col_names = T, col_types = cols(
+  itemID=col_factor(),
+  title=col_character(),
+  author=col_character(),
+  publisher=col_character(),
+  main.topic=col_character(),
+  subtopics=col_character(),
+  sessionID=col_factor(),
+  click=col_integer(),
+  basket=col_integer(),
+  order=col_integer()
+))
+head(joined_item_trans, n=20)
 
 # alternativ: mit Trimmer-Funktion --> Funktioniert noch nicht ganz? ):
 mainTopic_trimmer <- function(x) {
