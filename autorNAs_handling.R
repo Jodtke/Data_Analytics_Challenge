5b767b192ee52cf0ba2ac70f544a56a9db563d6d
Ludmila <ludmila.radiush@ku.de>
2021-05-20 20:49:42 +0200

    NAs in der Spalte "author" von 3247 auf 1336 reduziert, einige "publischer" auch groupiert

--- autorNAs_handling.R
+++ autorNAs_handling.R
@@ -11,7 +11,7 @@ getwd()
 ## Daten
 transactions_raw <- read.csv(file = "./Data/transactions.csv", header = T, sep = "|", quote = "", row.names = NULL, stringsAsFactors = F)
 dim(transactions_raw)  #365 143 x 5
-openRefine <- read.csv(file = "./Data/items2.csv", header = T, sep = ",", row.names = NULL, stringsAsFactors = F, encoding = "UTF-8")
+openRefine <- read.csv(file = "./Data/items3.csv", header = T, sep = ",", row.names = NULL, stringsAsFactors = F, encoding = "UTF-8")
 dim(openRefine) #78334 X 6
 evaluation <-  read.csv(file = "./Data/evaluation.csv", header = T, quote = "", row.names = NULL, stringsAsFactors = F)
 dim(evaluation)
@@ -29,25 +29,17 @@ dim(authorNAs)
 view(authorNAs)
 author_bearb <- right_join(authorNAs, evaluation_tbl)
 author_bearb <- drop_na(author_bearb)        # 62 rows tauchen in evaluation_DS auf
-
+count(oR_tbl, main.topic) %>% arrange(desc(n))
 #Inspect group_by Verlag
 #Arena Verlag: Malbücher, Kreuzworträtsel, Alter 5
 #Ars Edition: auch Alterempfehlung 5, von subtopics ablesbar
 #Coppenrath F: Stikerbuch, bastelbuch, malbuch, auch hier sind sie Ersatzprodukte
 #Yo Yo Books: gleiche main.topic --> also kann man auch Author durch Verlag ersetzen 
-#######################################################
-######################################################
 view(authorNAs) #group by publisher
 
 #######################################################
 #######################################################
 
-#inspect publisher Albert Whitman & Company
-view(authorNAs %>% filter(publisher== "Albert Whitman & Company")) #97 NAs
-view(oR_tbl %>% filter(publisher== "Albert Whitman & Company")) #173 Books from this publisher
-view(oR_tbl %>% filter(author == "Mike Litwin"))         #1 Book, same publisher
-view(oR_tbl %>% filter(author == "Linda Joy Singleton")) #2 Books, same publisher
-view(oR_tbl %>% filter(author == "Leslie Kimmelman"))    #3 Books, same publisher
 #replace NAs with Albert Whitman
 oR_tbl %>% filter(str_detect(oR_tbl$publisher, "^Albert"))
 oR_tbl <- transform(oR_tbl, 
@@ -113,20 +105,34 @@ view(authorNAs)
 #Books On Demand
 #Brianne Mitchell
 #Butzon & Bercker
-
 #Carlsen + Carlsen Verlag Gmbh
 #Castalia House
-
 #Childs Play + Child's Play International Ltd
 #Christophorus Verlag
 #Coppenrath and Coppenrath F
 #Dörfler + Dörfler Verlag Gmbh
-
 #Edition Michael Fischer
 #Fischer Kjb
 #Loewe Verlag Gmbh
 #St. Benno
 #Truant Ug
 #Panini Verlags Gmbh
+#White Star Verlag + White Star
+#Usborne Verlag + Usborne Publishing Ltd
+#Ullmann Medien+ Ullmann Medien Gmbh
+#Uhrwerk Verlag+Uhrwerk
+#Tessloff Verlag+Tessloff Medienvertrieb+Tessloff
+#Sweet Cherry Publishing
+#Stone Arch Books
+#Steck Vaughn Co + Star Trek
+#Simon Spotlight
+#Simon Pulse
+#Simon + Schuster Inc. + Simon & Schuster Books For Young Readers +Simon & Schuster
+#Schwager Und Steinlein
+#Schuenemann C.E.
+#Scholastic Ltd.+Scholastic
+#Schmid F. X.
+#Saint Philip Street Press
 
-write_csv(oR_tbl, file="./Data/items3.csv", col_names=T)
+
+#write_csv(oR_tbl, file="./Data/items3.csv", col_names=T)
